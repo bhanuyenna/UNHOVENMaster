6b9711d16bcf88dea7f0463750a80e9b367b9f82
gnurmatova <gnurmatova@identropy.com>
2016-02-15 19:24:31 -0500

    few more classes added

--- src/main/java/model/Recipe.java
+++ src/main/java/model/Recipe.java
@@ -1,10 +1,51 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Recipe {
+	private int number_of_servings;
+	private long recipe_id;
+	private ArrayList<Ingredient> ingredients;
+	private User author;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	public int getNumber_of_servings() {
+		return number_of_servings;
+	}
+
+
+	public void setNumber_of_servings(int number_of_servings) {
+		this.number_of_servings = number_of_servings;
+	}
+
+
+	public long getRecipe_id() {
+		return recipe_id;
+	}
+
+
+	public void setRecipe_id(long recipe_id) {
+		this.recipe_id = recipe_id;
+	}
+
+
+	public ArrayList<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+
+	public void setIngredients(ArrayList<Ingredient> ingredients) {
+		this.ingredients = ingredients;
+	}
+
+
+	public User getAuthor() {
+		return author;
+	}
+
+
+	public void setAuthor(User author) {
+		this.author = author;
 	}
 
 }
