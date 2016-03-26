c3799b6582ff691e915e676b5bf2b7209ad4ed46
bhanuyenna <yennabhanu1@gmail.com>
2016-03-25 20:27:14 -0400

    Update User.java

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,14 +1,69 @@
-package model;
+package main.java.model;
+import java.io.Serializable;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@Path("/User")
+@Produces("application/json")
 public class User {
-	private long user_id;
+	private int user_id;
+	private String firstName;
+	private String lastName;
+	private String passWord;
+	
+	public User()
+	{
+		
+	}
 
-	public long getUser_id() {
+	public int getUser_name() {
 		return user_id;
 	}
+	public void setUser_id(int i) {
+		this.user_id = i;
+	}
+	
 
-	public void setUser_id(long user_id) {
-		this.user_id = user_id;
+	public String getfirstName() {
+		return firstName;
 	}
 
+	public void setfirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	
+	public String getlastName() {
+		return firstName;
+	}
+
+	public void setlastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getpassWord() {
+		return firstName;
+	}
+
+	public void setpassWord(String firstName) {
+		this.firstName = firstName;
+	}
+
+	
+	
+	
+	
+
 }
+
+
+
